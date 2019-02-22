3837c18ef274a390a433602925348ad9f638f93f
alu@Zed-hp <farhim.ferdous3@gmail.com>
2019-02-22 20:23:13 +0600

    partly implemented responsive navbar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 // ELEMENTS //
 const navBtns = document.getElementsByClassName("nav-btn");
+const toggleBtn = document.getElementById("toggle-icon");
+const navBtnContainer = document.getElementsByClassName("nav-btn-container")[0];
 
 // EVENT LISTENERS //
-
+// active nav btn border
 for (const btn of navBtns) {
   btn.addEventListener("click", e => {
     if (!btn.classList.contains("active")) {
@@ -12,3 +14,8 @@ for (const btn of navBtns) {
     }
   });
 }
+
+// toggle-btn click
+toggleBtn.addEventListener("click", () => {
+  navBtnContainer.classList.toggle("collapse");
+});
